aec94374ba7fa19a687e877cd7fb66ba50d12b38
grantwilkins <grant.wilkins371@gmail.com>
2022-09-15 19:58:06 -0400

    final version

--- hw1/dot-product.cu
+++ hw1/dot-product.cu
@@ -6,50 +6,26 @@
 
 #define THREADS_PER_BLOCK 1024
 
-__global__ void GPU_big_dot(float *A, float *B, float *C, const long long N) {
+//Kernel function, create a
+__global__ void GPU_big_dot(float *A, float *B, float *C, long long N) {
 	
 	__shared__ float device_data[THREADS_PER_BLOCK];
-	unsigned int idx, stride, i;
-	idx = threadIdx.x + blockIdx.x * blockDim.x;
-	stride = blockDim.x*gridDim.x;
+	unsigned int idx, i;
+	float mult;
+	idx = threadIdx.x + blockIdx.x * blockDim.x; // Normal indexing
 
 	device_data[threadIdx.x] = A[idx] * B[idx];
 
-	
 	__syncthreads();
 
+	//Reduction done by the root thread in block
 	if(threadIdx.x == 0)
 	{
-		float mult = 0.0;
+		mult = 0.0;
 		for(i = 0; i < THREADS_PER_BLOCK; i++)
 			mult += device_data[i];
-		__syncthreads();
 		atomicAdd(C, mult);
 	}
-	
-	/*
-	float mult = 0.0;
-	while(idx < N)
-	{
-		mult += A[idx]*B[idx];	
-		idx += stride;
-	}
-	device_data[threadIdx.x] = mult;
-	
-	__syncthreads();
-
-	i = blockDim.x/2;
-	while(i != 0)
-	{
-		if(threadIdx.x < i)
-			device_data[threadIdx.x] += device_data[threadIdx.x + i];
-		__syncthreads();
-		i /= 2;
-	}
-
-	if(threadIdx.x == 0)
-		atomicAdd(C, device_data[0]);
-	*/
 }	
 
 
@@ -97,10 +73,16 @@ float CPU_big_dot(float *A, float *B, long long N) {
 
 int main(int argc, char ** argv) {
 
-	long long N_in = 100*512*512;
+	long long N = 100*512*512;
 	if(argc == 2)
-		N_in = atoi(argv[1]); // Allow user to set number of elements
-	const long long N = N_in;
+	{
+		N = atoi(argv[1]); // Allow user to set number of elements
+	}
+	else
+	{
+		printf("ATTN: Usage: ./dot-product <NUM_ELEMS>\n");
+		printf("Proceeding with default N = %lld\n\n", N);
+	}
 
 	//Initial variables
 	long long start_cpu = 0, start_gpu = 0, stop_cpu = 0, stop_gpu1 = 0, stop_gpu2 = 0, stop_gpu3 = 0;
@@ -125,20 +107,25 @@ int main(int argc, char ** argv) {
 	start_cpu = start_timer();
 	result_cpu = CPU_big_dot(v1, v2, N);
 	stop_cpu = stop_timer(start_cpu, name_cpu);
-	//dim3 dimGrid(128, 1, 1);
-    //dim3 dimBlock(32, 1, 1);
 
-    dim3 numBlocks((N+THREADS_PER_BLOCK-1)/THREADS_PER_BLOCK);
-	//Compute GPU
+
+	//This is to ensure that with some N < THREADS_PER_BLOCK we will
+	//still have a non-zero number of blocks.
+    	dim3 numBlocks((N+THREADS_PER_BLOCK-1)/THREADS_PER_BLOCK);
+	
 	start_gpu = start_timer();
 	//Copy the inputs to device
 	cudaMemcpy(device_v1, v1, size, cudaMemcpyHostToDevice);
 	cudaMemcpy(device_v2, v2, size, cudaMemcpyHostToDevice);
-    stop_gpu1 = stop_timer(start_gpu, name_gpu2);
-    start_gpu = start_timer();
+    	stop_gpu1 = stop_timer(start_gpu, name_gpu2);
+
+    	//Compute GPU
+    	start_gpu = start_timer();
 	GPU_big_dot<<<numBlocks,THREADS_PER_BLOCK>>>(device_v1, device_v2, device_result_gpu, N);
-    stop_gpu2 = stop_timer(start_gpu, name_gpu);
-    start_gpu = start_timer();
+    	stop_gpu2 = stop_timer(start_gpu, name_gpu);
+
+    	//Send data back
+    	start_gpu = start_timer();
 	cudaMemcpy(result_gpu,device_result_gpu,sizeof(float),cudaMemcpyDeviceToHost);
 	stop_gpu3 = stop_timer(start_gpu, name_gpu2);
 	
@@ -147,11 +134,14 @@ int main(int argc, char ** argv) {
 	printf("\nCPU Result: %e\n", result_cpu);
 	printf("GPU Result: %e\n", *result_gpu);
 	printf("\nSpeedup (with data transfer): %lf\n", (float)(stop_cpu)/ ((float)(stop_gpu1 + stop_gpu2 + stop_gpu3)));
-    printf("Speedup (just kernel): %lf\n", (float) stop_cpu / (float) stop_gpu2);
+    	printf("Speedup (just kernel): %lf\n", (float) stop_cpu / (float) stop_gpu2);
 	printf("\nRelative Error: %lf%%\n", 1e2*abs(*result_gpu - result_cpu) / (result_cpu));
 	//Cleanup and exit
 	cudaFree(device_v1);
 	cudaFree(device_v2);
 	cudaFree(device_result_gpu);
+	free(v1);
+	free(v2);
+	free(result_gpu);
 	return 0;
 }
