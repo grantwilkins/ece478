1bb875c96dd80beaf821bdc4a994ffc80345f976
grantwilkins <grant.wilkins371@gmail.com>
2022-11-01 20:44:37 -0400

    HW3 Update

--- hw3/dot-product.cu
+++ hw3/dot-product.cu
@@ -53,7 +53,6 @@ __global__ void kernel1(float *A, float *B, float *C, long long N) {
 
 	}
 
-
 	//Log our result for this block
 	if(threadIdx.x == 0)
 		C[blockIdx.x] = results[0];
@@ -62,7 +61,8 @@ __global__ void kernel1(float *A, float *B, float *C, long long N) {
 
 int main(int argc, char ** argv) {
 
-	int ngpus = 2;
+	int ngpus;
+	CHECK(cudaGetDeviceCount(&ngpus));
 	long long N = 1 << 24;
 	long long iSize = N/ngpus;
 	long long iBytes = iSize*sizeof(float);
@@ -76,7 +76,7 @@ int main(int argc, char ** argv) {
 	float **v1, **v2, **result_ker1; // host copies
 	float **dev_v1, **dev_v2, **dev_ker1; // device copies
 
-	//Double array implementation
+	//Double array allocation
 	v1 = (float **) malloc(sizeof(float *)*ngpus);
 	v2 = (float **) malloc(sizeof(float *)*ngpus);
 	dev_v1 = (float **) malloc(sizeof(float *)*ngpus);
@@ -85,33 +85,42 @@ int main(int argc, char ** argv) {
 	dev_ker1 = (float **) malloc(sizeof(float * )*ngpus);
 	cudaStream_t *stream = (cudaStream_t *)malloc(sizeof(cudaStream_t) * ngpus);
 	
+	double iStart = seconds();
 	//Allocate memory on each GPU and generate random vectors;
 	for(i = 0; i < ngpus; i++)
 	{
-		//set current device
+		// set current device
 		CHECK(cudaSetDevice(i));
 		
+		// get device memory
 		CHECK(cudaMalloc((void **)&dev_v1[i], iBytes));
 		CHECK(cudaMalloc((void **)&dev_v2[i], iBytes));
 		CHECK(cudaMalloc((void **)&dev_ker1[i], sizeof(float)*numBlocks));
-		result_ker1[i] = (float *) malloc(sizeof(float)*numBlocks);
 
+		// allocate page locked host memory for asynchronous data transfer
 		CHECK(cudaMallocHost((void **) &v1[i], iBytes));
 		CHECK(cudaMallocHost((void **) &v2[i], iBytes));
 		CHECK(cudaMallocHost((void **) &result_ker1[i], numBlocks*sizeof(float)));
-		CHECK(cudaStreamCreate(&stream[i]));
 
+		// create streams for timing and synchronizing
+		CHECK(cudaStreamCreate(&stream[i]));
+	}
+
+	for(i = 0; i < ngpus; i++)
+	{
+		CHECK(cudaSetDevice(i));
 		get_random_vector(v1[i], iSize);
 		get_random_vector(v2[i], iSize);
 	}
 
+	// distributing the workload across multiple devices
 	for(i = 0; i < ngpus; i++)
 	{
 		CHECK(cudaSetDevice(i));
 		CHECK(cudaMemcpyAsync(dev_v1[i], v1[i], iBytes, cudaMemcpyHostToDevice, stream[i]));
 		CHECK(cudaMemcpyAsync(dev_v2[i], v2[i], iBytes, cudaMemcpyHostToDevice, stream[i]));
 		
-		kernel1<<<numBlocks,THREADS_PER_BLOCK>>>(dev_v1[i], dev_v2[i], dev_ker1[i], iSize);
+		kernel1<<<numBlocks,THREADS_PER_BLOCK,0,stream[i]>>>(dev_v1[i], dev_v2[i], dev_ker1[i], iSize);
 		
 		CHECK(cudaMemcpyAsync(result_ker1[i],dev_ker1[i],numBlocks*sizeof(float),
 			cudaMemcpyDeviceToHost, stream[i]));
@@ -123,6 +132,9 @@ int main(int argc, char ** argv) {
       	CHECK(cudaSetDevice(i));
       	CHECK(cudaStreamSynchronize(stream[i]));
     	}
+
+    	double iElaps = seconds() - iStart;
+    	printf("%d GPU timer elapsed: %8.2fms \n", ngpus, iElaps * 1000.0);
 	
 	for(i = 0; i < ngpus; i++)
 	{
@@ -132,12 +144,29 @@ int main(int argc, char ** argv) {
 	
 	printf("Kernel Result: %e\n",  final_ker1);
 
+	// Cleanup and shutdown
+	for (int i = 0; i < ngpus; i++)
+	{
+		CHECK(cudaSetDevice(i));
+		CHECK(cudaFree(dev_v1[i]));
+		CHECK(cudaFree(dev_v2[i]));
+		CHECK(cudaFree(dev_ker1[i]));
+
+		CHECK(cudaFreeHost(result_ker1[i]));
+		CHECK(cudaFreeHost(v1[i]));
+		CHECK(cudaFreeHost(v2[i]));
+		CHECK(cudaStreamDestroy(stream[i]));
+
+		CHECK(cudaDeviceReset());
+	}
+
 	//Cleanup and exit
-	cudaFree(dev_v1);
-	cudaFree(dev_v2);
-	cudaFree(dev_ker1);
+	free(dev_v1);
+	free(dev_v2);
+	free(dev_ker1);
 	free(v1);
 	free(v2);
+	free(stream);
 	free(result_ker1);
 	return 0;
 }
